950ccf3963f03676acb963c1ff828831aabd5926
LeonGuerreroM <leonmora19@outlook.com>
2022-03-16 23:42:02 -0600

    cors, procfile, engine: getting ready for heroku

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require('express');
-//const cors = require('cors');
+const cors = require('cors');
 const routerApi = require('./routes');
 const { ormErrorHandler, logErrors, errorHandler } = require('./utils/middlewares/errorHandlers');
 
 const app = express();
-//const port = process.env.PORT || 3001;
-const port = 3002;
+const port = process.env.PORT || 3002;
+
 
 app.use(express.json());
+
+const whitelist = ['http://localhost:8080', 'http://myapp.com'];
+const options = {
+  origin: (origin, callback) => {
+    if(whitelist.includes(origin) || !origin){
+      callback(null, true);
+    }else{
+      callback(new Error('Access denied'));
+    }
+  }
+}
+app.use(cors(options));
 
 app.get('/', (req, res) => {
   res.send("Solicitud de recursos materiales");
