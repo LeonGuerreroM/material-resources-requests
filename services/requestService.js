7e466a414ff0b7b083485d4e7f5b570668ee0617
LeonGuerreroM <leonmora19@outlook.com>
2022-03-16 23:25:43 -0600

    request pagination

--- services/requestService.js
+++ services/requestService.js
@@ -1,10 +1,28 @@
 const { models } = require('../libs/sequelize');
+const { Op } = require('sequelize');
+const { options } = require('pg/lib/defaults');
 
 class RequestServices{
   constructor() {}
 
   async find(query){
-    const info = await models.Request.findAll();
+    const options = {
+      //include: ['category'],
+      where: {}
+    }
+
+    const { status } = query;
+    if(status == 1){
+      options.where.validated = { [Op.eq]: true };
+    }else if(status == 2){
+      options.where.validated = { [Op.eq]: false };
+    }else if(status == 3){
+      options.where.processed = { [Op.eq]: true };
+    }else if(status == 4){
+      options.where.processed = { [Op.eq]: false };
+    }
+
+    const info = await models.Request.findAll(options);
     return info;
   }
 
@@ -14,6 +32,9 @@ class RequestServices{
   }
 
   async create(data){
+    if(data.validated){
+      console.log('trajo '+ data.validated);
+    }
     const newElement = await models.Request.create(data);
     return newElement;
   }
