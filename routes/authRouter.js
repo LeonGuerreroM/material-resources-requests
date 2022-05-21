f8c1918678f165ee5dc4d571d95ff1a74130974f
LeonGuerreroM <leonmora19@outlook.com>
2022-05-21 18:12:15 -0500

    documentation added to routes and validateRequests route moved above processRequests route

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -5,6 +5,27 @@ const { config } = require('../config/config');
 
 const router = express.Router();
 
+  /**
+   * @module authenticationRoutes
+   */
+
+  /**
+   * @name login
+   * @path {POST} /api/v1/auth/login
+   *
+   * @body {String} username user's registered username
+   * @body {String} password
+   *
+   * @response {Object}
+   * @response {String} object.user authenticated user
+   * @response {String} object.token bearer token needed to access every function related to the user
+   *
+   * @code {200} if the credentials were correct
+   * @code {400} in case of wrong body parameters
+   * @code {401} in case of wrong credentials
+   * @code {500} in case of internal errors with the request
+   *
+   */
 router.post('/login',
   passport.authenticate('local', {session:false}),
   async(req, res, next) => {
